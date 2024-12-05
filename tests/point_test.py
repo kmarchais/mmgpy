d40d0fde1214435cd6670c6c44e73ffc0397c115
Kevin Marchais <kevinmarchais@gmail.com>
2024-12-05 20:33:31 +0100

    fix point test

--- tests/point_test.py
+++ tests/point_test.py
@@ -15,10 +15,19 @@ def test_basic_usage() -> None:
     p.c = coords
     assert np.allclose(p.c, coords)
 
-    # Test string representation
-    assert repr(p) == f"<MMG5_Point at ({coords})>"
-
     # Test reference setting
     ref = 42
     p.ref = ref
     assert p.ref == ref
+
+
+def test_point_repr() -> None:
+    """Test the __repr__ method of the Point class."""
+    p = Point()
+    assert str(p) == "<MMG5_Point at (0.000000,0.000000,0.000000)>"
+
+    p.c = np.array([1.0, 2.5, -3.7])
+    assert repr(p) == "<MMG5_Point at (1.000000,2.500000,-3.700000)>"
+
+    p.c = np.array([0.1, 0.0, -0.1])
+    assert str(p) == "<MMG5_Point at (0.100000,0.000000,-0.100000)>"
