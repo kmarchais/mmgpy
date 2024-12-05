822cce58b8db955e16110b6b11d10f304738bcb3
Kevin Marchais <kevinmarchais@gmail.com>
2024-12-05 21:33:48 +0100

    add Tetra class

--- src/mmgpy/__init__.py
+++ src/mmgpy/__init__.py
@@ -1,7 +1,7 @@
 """Python bindings for the MMG library."""
 
 from . import _version  # type: ignore[attr-defined]
-from ._mmgpy import MMG_VERSION, Point  # type: ignore[attr-defined]
+from ._mmgpy import MMG_VERSION, Point, Tetra  # type: ignore[attr-defined]
 from .remesh import mmg2d, mmg3d, mmgs
 
 __version__ = _version.__version__
@@ -9,6 +9,7 @@ __version__ = _version.__version__
 __all__ = [
     "MMG_VERSION",
     "Point",
+    "Tetra",
     "__version__",
     "mmg2d",
     "mmg3d",
