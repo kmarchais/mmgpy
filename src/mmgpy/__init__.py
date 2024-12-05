690051b57f228037cdaf30bdfa6c08ceab137926
Kevin Marchais <kevinmarchais@gmail.com>
2024-12-05 21:54:21 +0100

    add Mesh class

--- src/mmgpy/__init__.py
+++ src/mmgpy/__init__.py
@@ -1,13 +1,14 @@
 """Python bindings for the MMG library."""
 
 from . import _version  # type: ignore[attr-defined]
-from ._mmgpy import MMG_VERSION, Point, Tetra  # type: ignore[attr-defined]
+from ._mmgpy import MMG_VERSION, Mesh, Point, Tetra  # type: ignore[attr-defined]
 from .remesh import mmg2d, mmg3d, mmgs
 
 __version__ = _version.__version__
 
 __all__ = [
     "MMG_VERSION",
+    "Mesh",
     "Point",
     "Tetra",
     "__version__",
