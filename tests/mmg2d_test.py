f3b9833681b9c8c85f9df66750521cf3e98208a8
Kevin Marchais <kevinmarchais@gmail.com>
2024-12-21 21:02:02 +0100

    debug

--- tests/mmg2d_test.py
+++ tests/mmg2d_test.py
@@ -1,6 +1,6 @@
 """Tests for the MMG2D Python wrapper."""
 
-# ruff: noqa: ERA001, RUF100, F401
+# ruff: noqa: ERA001, RUF100, F401, F841
 
 import platform
 import subprocess
@@ -9,28 +9,29 @@ from pathlib import Path
 from mmgpy import mmg2d
 
 
-# def test_mmg2d() -> None:
-#     """Test that the Python wrapper produces the same output as the executable."""
-#     exe = "mmg2d.exe" if platform.system() == "Windows" else "mmg2d_O3"
-#     input_mesh = Path(__file__).parent.parent / "assets" / "acdcBdy.mesh"
+def test_mmg2d() -> None:
+    """Test that the Python wrapper produces the same output as the executable."""
+    exe = "mmg2d.exe" if platform.system() == "Windows" else "mmg2d_O3"
+    input_mesh = Path(__file__).parent.parent / "assets" / "acdcBdy.mesh"
 
-#     current_dir = Path(__file__).parent
-#     test_path = current_dir / "acdcBdy_remeshed.vtk"
-#     ref_path = current_dir / "acdcBdy.o.vtk"
+    current_dir = Path(__file__).parent
+    test_path = current_dir / "acdcBdy_remeshed.vtk"
+    ref_path = current_dir / "acdcBdy.o.vtk"
 
-#     ar = 10
-#     hmax = 10
-#     verbose = -1
+    ar = 10
+    hmax = 10
+    verbose = -1
 
-#     mmg2d.remesh(
-#         input_mesh=str(input_mesh),
-#         output_mesh=str(test_path),
-#         options={
-#             "ar": ar,
-#             "hmax": hmax,
-#             "verbose": verbose,
-#         },
-#     )
+    mmg2d.remesh(
+        input_mesh=str(input_mesh),
+        output_mesh=str(test_path),
+        options={
+            "ar": ar,
+            "hmax": hmax,
+            "verbose": verbose,
+        },
+    )
+
 
 #     command = [
 #         exe,
