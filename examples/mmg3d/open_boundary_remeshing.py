f67a20a58f2172b877d012b3aaf8fb2f4a1d6f6b
Kevin Marchais <kevinmarchais@gmail.com>
2025-01-02 09:58:55 +0100

    update 3D examples

--- examples/mmg3d/open_boundary_remeshing.py
+++ examples/mmg3d/open_boundary_remeshing.py
@@ -20,21 +20,23 @@ from mmgpy import mmg3d
 INPUT_FILE = Path(__file__).parent.parent.parent / "assets" / "island.mesh"
 OUTPUT_FILE = Path(__file__).parent / "output.vtk"
 
-mmg3d.remesh(
-    input_mesh=str(INPUT_FILE),
-    output_mesh=str(OUTPUT_FILE),
-    options={
-        "opnbdy": 1,
-        "verbose": -1,
-    },
-)
 
-mesh = pv.read(OUTPUT_FILE)
+pl = pv.Plotter(shape=(1, 2), window_size=(800, 400))
+for open_boundary in [False, True]:
+    pl.subplot(0, int(open_boundary))
+    mmg3d.remesh(
+        input_mesh=str(INPUT_FILE),
+        output_mesh=str(OUTPUT_FILE),
+        options={
+            "opnbdy": open_boundary,
+            "verbose": -1,
+        },
+    )
 
-pl = pv.Plotter()
-pl.add_mesh(
-    mesh.extract_cells(mesh.cell_centers().points[:, :] < 0),
-    scalars="mesh.sol:metric",
-    show_edges=True,
-)
+    mesh = pv.read(OUTPUT_FILE)
+    pl.add_mesh(
+        mesh.extract_cells(mesh.cell_centers().points[:, :] < 0),
+        show_edges=True,
+    )
+    pl.add_text(f"Open boundary: {open_boundary}")
 pl.show()
