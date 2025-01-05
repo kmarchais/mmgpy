b79b32e00a99ab5be7127bda3de81c95bcb42d94
Kevin Marchais <kevinmarchais@gmail.com>
2025-01-05 21:46:09 +0100

    add MmgMesh class

--- src/bindings/bindings.cpp
+++ src/bindings/bindings.cpp
@@ -1,8 +1,12 @@
 #include "bindings.h"
-
 #include "mmg/common/mmgversion.h"
+#include "mmg_mesh.hpp"
 
 PYBIND11_MODULE(_mmgpy, m) {
+  // Add the new MmgMesh class
+  py::class_<MmgMesh>(m, "MmgMesh").def(py::init<>());
+
+  // Keep existing bindings
   py::class_<mmg3d>(m, "mmg3d")
       .def_static("remesh", remesh_3d, py::arg("input_mesh"),
                   py::arg("input_sol") = "", py::arg("output_mesh") = "",
